c6382d615eaa35e61f2808e0920d24381cdc0f92
Zsuzsanna Ver <56695526+MrsVer@users.noreply.github.com>
2021-06-21 13:36:09 +0100

    Add update bookmarks function

--- app.rb
+++ app.rb
@@ -1,36 +1,35 @@
 require 'sinatra/base'
-require 'sinatra/reloader'
-require './lib/bookmark'
-
+require_relative './lib/bookmark'
 class BookmarkManager < Sinatra::Base
-  configure :development do
-    register Sinatra::Reloader
-  end
-
   enable :sessions, :method_override
-
   get '/' do
     'Bookmark Manager'
   end
-
   get '/bookmarks' do
     @bookmarks = Bookmark.all
     erb :'bookmarks/index'
   end
-
   get '/bookmarks/new' do
     erb :'bookmarks/new'
   end
-
   post '/bookmarks' do
     Bookmark.create(url: params[:url], title: params[:title])
     redirect '/bookmarks'
   end
-
   delete '/bookmarks/:id' do
     Bookmark.delete(id: params[:id])
     redirect '/bookmarks'
   end
 
-  run if app_file == $0
+  get '/bookmarks/:id/edit' do
+    @bookmark = Bookmark.find(id: params[:id])
+    erb :"bookmarks/edit"
+  end
+
+  patch '/bookmarks/:id' do
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
+    redirect('/bookmarks')
+  end
+
+  run! if app_file == $0
 end
