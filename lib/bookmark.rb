afb459f1e84994b556c7db0a48a669e6ec24e5c1
Zsuzsanna Ver <56695526+MrsVer@users.noreply.github.com>
2021-06-26 10:04:58 +0100

    Debugging

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,15 +2,6 @@ require_relative 'database_connection'
 require 'uri'
 
 class Bookmark
-
-  attr_reader :id, :title, :url
-
-  def initialize(id:, title:, url:)
-    @id = id
-    @title = title
-    @url = url
-  end
-
   def self.all
     result = DatabaseConnection.query("SELECT * FROM bookmarks")
     result.map do |bookmark|
@@ -27,19 +18,22 @@ class Bookmark
     result = DatabaseConnection.query("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, url;")
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
-
   def self.delete(id:)
     DatabaseConnection.query("DELETE FROM bookmarks WHERE id = #{id}")
   end
-
   def self.update(id:, title:, url:)
     result = DatabaseConnection.query("UPDATE bookmarks SET url = '#{url}', title = '#{title}' WHERE id = #{id} RETURNING id, url, title;")
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
-
   def self.find(id:)
     result = DatabaseConnection.query("SELECT * FROM bookmarks WHERE id = #{id}")
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+  end
+  attr_reader :id, :title, :url
+  def initialize(id:, title:, url:)
+    @id = id
+    @title = title
+    @url = url
   end
 
   private
